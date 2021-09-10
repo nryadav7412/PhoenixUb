2bf834d34e84f53a0e0510577758be8244e1f2d1
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2021-09-10 17:29:39 +0530

    Create __main__.py

---
+++ PhoenixUb/__main__.py
@@ -0,0 +1,11 @@
+from PhoenixUb import phoenixub 
+import logging  
+import Rimuru.modules 
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+
+logger = logging.getLogger("__name__")
+
+print("Starting....")
+rimuru.start()
+rimuru.run_until_disconnected()
