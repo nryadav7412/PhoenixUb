cd43c96e8fa1e4365980c1346f5915e6d1b73e60
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2021-09-10 17:42:15 +0530

    Create pm_sql.py

---
+++ PhoenixUb/modules/sql/pm_sql.py
@@ -0,0 +1,45 @@
+import threading
+from PhoenixUb.modules.sql import BASE, SESSION
+from sqlalchemy import (Column, ForeignKey, Integer, String, UnicodeText, UniqueConstraint, func)
+
+class Users(BASE):
+  __tablename__ = "users"
+  userid = Column(Integer, primary_key=True)
+  
+  def __init__(self, userid):
+    self.userid = userid
+ 
+Users.__table__.create(checkfirst=True)
+INSERTION_LOCK = threading.RLock() 
+
+def is_approved(userid):
+    try:
+      user = SESSION.query(Users).get(userid)
+      if user:
+        return True 
+      else:
+        return False
+    finally:
+        SESSION.close()
+  
+def approve(userid):
+  with INSERTION_LOCK:
+    user = SESSION.query(Users).get(userid)
+    if user:
+      SESSION.close()
+      return False
+    else:
+      user = Users(userid)
+      SESSION.add(user)
+      SESSION.commit()
+      return True
+  
+def disapprove(userid):
+  with INSERTION_LOCK:
+    user = SESSION.query(Users).get(userid)
+    if user:
+      SESSION.delete(user)
+      SESSION.commit()
+      return True 
+    else: 
+      return False
