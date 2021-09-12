7eef9c017af9bacca73cbdda05a03ea462052377
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2021-09-12 17:31:54 +0530

    Update dm.py

--- PhoenixUb/modules/dm.py
+++ PhoenixUb/modules/dm.py
@@ -3,28 +3,13 @@ from telethon import events, Button, functions, types
 import asyncio
 
 @phoenixub.on(events.NewMessage(outgoing=True , pattern=".dm ?(.*)"))
-async def dm(e):
-    if len(e.text) > 3:
-        if not e.text[3] == " ":  # weird fix
-            return
-    d = e.pattern_match.group(1)
-    c = d.split(" ")
-    try:
-        chat_id = await get_user_id(c[0])
-    msg = ""
-    masg = await e.get_reply_message()
-    if e.reply_to_msg_id:
-        await phoenixub.send_message(chat_id, masg)
-        await e.edit("`Message Sent!`")
-    for i in c[1:]:
-        msg += i + " "
-    if msg == "":
-        return
-    try:
-        await phoenixub.send_message(chat_id, msg)
-        await e.edit("`Message Sent!`")
-    except BaseException:
-        await e.edit(f"Use Properly Saar : `.dm <reply>/.dm <username> <msg>`")
+async def dm(slime):
+    args=slime.pattern_match.group(1)
+    args=args.split(' ')
+    if args[0]:
+        reply=await slime.get_reply_message()
+        await phoenixub.send(args[0] , reply)
+    
 
 @phoenixub.on(events.NewMessage(outgoing=True , pattern=".save"))
 async def save(slime):
