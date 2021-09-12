5f6f844c6e040483dfe76d4a89b2198f156b4a63
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2021-09-12 16:36:09 +0530

    Update pm.py

--- PhoenixUb/modules/pm.py
+++ PhoenixUb/modules/pm.py
@@ -24,16 +24,16 @@ async def pmperm(slime):
   if user.id in now:
     count = now[user.id] 
     if count <= 5:
-      await phoenixub.send_message(slime.chat_id,f"**Stop Spamming Saar** \n**Your Warns** : {count}")
+      await phoenixub.send_message(slime.chat_id,f"**Stop Spamming Saar** \n**Your Warns** : {count}/7")
       now[user.id] = count + 1
     elif count == 6:
-      await phoenixub.send_message(slime.chat_id,"Spam More , This is Your Last Chance")
+      await phoenixub.send_message(slime.chat_id,"Spam More hehehe .., This is Your Last Chance😌")
       now[user.id] = count + 1
     elif count== 7:
       await phoenixub(functions.contacts.BlockRequest(id=user.id))
-      await phoenixub.send_message(slime.chat_id, "Agh, I Told You Not to Spam Now You are Blocked heheh.")
+      await phoenixub.send_message(slime.chat_id, "Ewee, I Told You Not to Spam Now You are Blocked huh😒!.")
       time.sleep(2)
-      await phoenixub.send_message(slime.chat_id, "Wait till My Master comes..")
+      await phoenixub.send_message(slime.chat_id, "Wait till My Master comes to approve you and found something useful in you ..")
       now[user.id] = 0
   else:
     now[user.id] = 0
@@ -44,15 +44,16 @@ async def approve(slime):
   if not slime.is_private:
     return 
   await slime.edit("✿Approving...")
+  time.sleep(3)
   user = await slime.get_chat()
   h = pm.approve(user.id)
   if h == False:
     await slime.edit("⍟Already aprovved..")
-    time.sleep(2)
+    time.sleep(3)
     await slime.delete()
     return
   await slime.edit("✧Successful!")
-  time.sleep(2)
+  time.sleep(3)
   await slime.delete()
 
 @phoenixub.on(events.NewMessage(outgoing=True, pattern=r"^.(d|disapprove)($)"))    
@@ -60,13 +61,14 @@ async def approve(slime):
   if not slime.is_private:
     return 
   await slime.edit("✿Disapproving...")
+  time.sleep(3)
   user = await slime.get_chat()
   h = pm.disapprove(user.id)
   if h == False:
     await slime.edit("⍟Already disaprovved..")
-    time.sleep(2)
+    time.sleep(3)
     await slime.delete()
     return
   await slime.edit("✧Successful!")
-  time.sleep(2)
+  time.sleep(3)
   await slime.delete()
