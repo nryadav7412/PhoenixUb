dfa15a9f6ded4eb55ce1e81b63031a26892fefbc
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2022-03-18 14:50:04 +0530

    Update channelapprover.py

--- PhoenixUb/modules/channelapprover.py
+++ PhoenixUb/modules/channelapprover.py
@@ -21,3 +21,26 @@ Regard ~ t.me/Latest_Movies_And_Series**''')
           await phoenix.send_message(-1001287542359,f'''**@Legends_Nvr_Die
 #NEW_APPROVE {chat_.title}
 Approved New User : [{who.first_name}](tg://user?id={event.user_id})**''')
+          
+@phoenixub.on(events.NewMessage(chats=-1001216630749))
+async def my_event_handler(event):
+    if event.video:
+        a=event.media
+        h=event.text
+        if 'APDBackup' in event.text:
+            h=h.replace('APDBackup','Latest_Movies_And_Series')
+        elif 'AlPacinoDump' in event.text:
+            h=h.replace('AlPacinoDump','Latest_Movies_And_Series')
+        elif 'alpacinodump' in event.text:
+            h=h.replace('alpacinodump','Latest_Movies_And_Series')
+        await phoenixub.send_file(-1001504320073,event.media,caption=h)
+    elif event.text:
+        if not event.photo:
+            h=event.text
+            if 'APDBackup' in event.text:
+                h=h.replace('APDBackup','Latest_Movies_And_Series')
+            elif 'AlPacinoDump' in event.text:
+                h=h.replace('AlPacinoDump','Latest_Movies_And_Series')
+            elif 'alpacinodump' in event.text:
+                h=h.replace('alpacinodump','Latest_Movies_And_Series')
+            await phoenixub.send_message(-1001504320073,h)
