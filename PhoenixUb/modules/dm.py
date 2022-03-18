3ea5c3fece043e80bdd9fdcc3fa2ff62054cc3ec
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2022-03-18 14:31:06 +0530

    Update dm.py

--- PhoenixUb/modules/dm.py
+++ PhoenixUb/modules/dm.py
@@ -33,10 +33,15 @@ async def sco(slime):
     for i in range (count):
           await phoenixub.send_message(slime.chat_id,reply)
 
-@phoenixub.on(events.NewMessage(outgoing=True , pattern=".move"))
-async def dm(slime):
-        reply=await slime.get_reply_message()
-        await phoenixub.send_message(-1001315396403 , reply)
-        await slime.edit("`Movie Sent in Channel!`")
-        await asyncio.sleep(3)
-        await slime.delete()
+@phoenixub_on(events.NewMessage(outgoing=True , pattern=".lmas"))
+async def semd(e):
+     reply=await e.get_reply_message()
+     await e.delete()
+     reply_id=reply.id
+     from_id=reply.id - 1
+     msgss=[]
+     async for msg in phoenixub.iter_messages(e.chat_id, reverse=True , offset_id=from_id):
+          msgss.append(msg)
+     for i in msgss[::-1]:
+          if i.video:
+            await phoenixub.send_message(-1001598947835,i)
