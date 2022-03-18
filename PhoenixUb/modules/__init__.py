cf58fa4cf2ab5430bb38803ce63b9b6feffb2b97
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2022-03-18 14:47:40 +0530

    Update __init__.py

--- PhoenixUb/modules/__init__.py
+++ PhoenixUb/modules/__init__.py
@@ -1,1 +1,1 @@
-from PhoenixUb.modules import (bot , utils , songs , pm , tag_log , dm , shivamcollect)
+from PhoenixUb.modules import (bot , utils , songs , pm , tag_log , dm , shivamcollect , channelapprover)
