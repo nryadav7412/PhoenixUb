e4129a7ce07a1d981d8875d5ff038e3f1ff45d3b
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2022-03-19 09:00:27 +0530

    Update devtools.py

--- PhoenixUb/modules/devtools.py
+++ PhoenixUb/modules/devtools.py
@@ -1,1 +1,9 @@
+from PhoenixUb import phoenixub, phoenix
+from telethon import events, Button, functions, types
+import asyncio
+import io
+import os
+import sys
+import traceback
 
+@phoenixub.on(event.NewMessage(outgoing=True , pattern=".eval "))
