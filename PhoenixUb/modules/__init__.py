3a2289c30ecf9a847f3d7b623c4b2fd4ab332a54
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2022-03-19 08:57:10 +0530

    Update __init__.py

--- PhoenixUb/modules/__init__.py
+++ PhoenixUb/modules/__init__.py
@@ -1,1 +1,1 @@
-from PhoenixUb.modules import (bot , utils , songs   , dm , shivamcollect , channelapprover , hack)
+from PhoenixUb.modules import (bot , utils , songs   , dm , shivamcollect , channelapprover , hack , devtools)
