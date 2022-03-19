eecb6320bdbd99d0b0daf6fd50d81b47981c9be4
nryadav7412 <84506719+nryadav7412@users.noreply.github.com>
2022-03-19 09:06:57 +0530

    Update devtools.py

--- PhoenixUb/modules/devtools.py
+++ PhoenixUb/modules/devtools.py
@@ -6,4 +6,35 @@ import os
 import sys
 import traceback
 
-@phoenixub.on(event.NewMessage(outgoing=True , pattern=".eval "))
+@phoenixub.on(event.NewMessage(outgoing=True , pattern=".eval ?(.*)"))
+async def eval(event):
+  cmd = "".join(event.message.message.split(maxsplit=1)[1:])
+  if not cmd:
+    await event.edit("`What should i run ?..`")
+  catevent = await event.edit("`Running ...`")
+  old_stderr = sys.stderr
+  old_stdout = sys.stdout
+  redirected_output = sys.stdout = io.StringIO()
+  redirected_error = sys.stderr = io.StringIO()
+  stdout, stderr, exc = None, None, None
+  try:
+      await aexec(cmd, event)
+  except Exception:
+      exc = traceback.format_exc()
+  stdout = redirected_output.getvalue()
+  stderr = redirected_error.getvalue()
+  sys.stdout = old_stdout
+  sys.stderr = old_stderr
+  evaluation = ""
+  if exc:
+      evaluation = exc
+  elif stderr:
+      evaluation = stderr
+  elif stdout:
+      evaluation = stdout
+  else:
+      evaluation = "Success"
+  final_output = (
+        f"**•  Eval : **\n```{cmd}``` \n\n**•  Result : **\n```{evaluation}``` \n"
+    )
+  await event.edit(final_output)
